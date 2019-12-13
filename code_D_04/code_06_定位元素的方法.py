1b003f613602dc8fd0542fe0e62cf5c37941845e
ChoCandyT <381110118@qq.com>
2019-12-13 18:09:05 +0800

    modify code_06 add driver.find_element(By.ID, "xxxxx")

--- code_D_04/code_06_定位元素的方法.py
+++ code_D_04/code_06_定位元素的方法.py
@@ -4,6 +4,7 @@
 
 # 导入python需要的webdriver库
 from appium import webdriver
+from selenium.webdriver.common.by import By
 
 # 定义一个空字典存放具体的配置参数
 desired_caps = dict()
@@ -19,13 +20,19 @@ desired_caps["appActivity"] = ".Settings"
 driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_caps)
 
 # 点击设置界面的 搜索按钮
-driver.find_element_by_id("com.android.settings:id/search").click()
+# driver.find_element_by_id("com.android.settings:id/search").click()
+search_btn = (By.ID, 'com.android.settings:id/search')
+driver.find_element(*search_btn).click()
 
 # 点击搜索功能的 返回按钮
-driver.find_element_by_class_name("android.widget.ImageButton").click()
+# driver.find_element_by_class_name("android.widget.ImageButton").click()
+back_btn = (By.CLASS_NAME, 'android.widget.ImageButton')
+driver.find_element(*back_btn).click()
 
 # 点击设置页面的 WLAN按钮
-driver.find_element_by_xpath("//*[@text='WLAN']").click()
+# driver.find_element_by_xpath("//*[@text='WLAN']").click()
+wlan_btn = (By.XPATH, "//*[@text='WLAN']")
+driver.find_element(*wlan_btn).click()
 
 # 关闭资源
 driver.quit()
